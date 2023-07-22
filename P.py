6993fd0dd61305e5723e4d9335bcf7f5f4c7cdab
Luis <lmrtnz36@gmail.com>
2023-07-22 12:41:37 -0400

    cambio de nombre

--- P.py
+++
@@ -1,9 +0,0 @@
-Peso = input("Ingrese su peso:")
-peso_total = int(Peso) 
-
-Altura = input("Ingrese su altura:")
-altura_total = float(Altura)
-
-imc = peso_total / altura_total**2
-
-print("Tu indice de masa corporal:",imc)
